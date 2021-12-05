ddecc71f8d39bb2636f36a2a15cdaa32e9bda6b6
Tony Bowden <tony@tmtm.com>
2021-12-05 21:16:39 +0200

    add script to mirror official site

---
+++ bin/mirror.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf mirror
+mkdir mirror
+cd mirror
+
+CURLOPTS='-L -c /tmp/cookies -A eps/1.2'
+
+curl $CURLOPTS -o ministers-1.html "https://www.parliament.gov.zm/ministers/cabinet"
+curl $CURLOPTS -o ministers-2.html "https://www.parliament.gov.zm/ministers/cabinet?page=1"
+curl $CURLOPTS -o ministers-3.html "https://www.parliament.gov.zm/ministers/cabinet?page=2"
+
+cd -
